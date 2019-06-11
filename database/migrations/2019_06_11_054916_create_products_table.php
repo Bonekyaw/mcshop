9bba2271ab10678201d428929f483cd2b4a0e961
Phone Nyo <phonenai2014@gmail.com>
2019-06-11 17:33:38 +0630

    Migration Table

---
+++ database/migrations/2019_06_11_054916_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('product');
+            $table->unsignedBigInteger('brand_id');
+            $table->unsignedBigInteger('category_id');
+            $table->float('price',8,2);
+            $table->float('discount',8,2)->nullable();
+            $table->text('description')->nullable();
+            $table->text('benefit')->nullable();
+            $table->string('weight')->nullable();
+            $table->string('photo')->nullable();
+            $table->integer('inStock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
